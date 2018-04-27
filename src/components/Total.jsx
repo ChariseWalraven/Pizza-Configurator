edbcd8102a4b8f63d5c39356df9003e6db8ebe54
Charise Walraven <cherri.walraven@gmail.com>
2018-04-27 19:28:10 +0200

    Add missing prevent default

--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,13 +1,17 @@
 import React, {PureComponent} from 'react'
 
 export default class Total extends PureComponent {
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
   render() {
     return(
       <form>
         <img className='yummy-pizza' alt='yummy-pizza' 
         src='https://images.unsplash.com/photo-1511516457495-002cdd242a2e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1c70df78467f2a0b0f5a3d5d07ee1a69&auto=format&fit=crop&w=1500&q=80'/>
         <span className='total'>&euro;{}</span>
-        <button>Order</button>
+        {/* Link to a new page with a tick? toggle hidden class or something when clicked? */}
+        <button onClick={(e) => this.handleSubmit(e)}>Order</button>
       </form>
     )
   }
