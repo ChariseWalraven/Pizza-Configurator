6df416fdb13730bb5c259aa96ea9eef61358aeff
Charise Walraven <cherri.walraven@gmail.com>
2018-04-27 03:35:26 +0200

    Can see straight anymore. I give up for tonight.

--- src/logic/logic.js
+++ src/logic/logic.js
@@ -18,9 +18,24 @@ else return Object.values(sauces[id])[0]
 
 //implement your form and see what you get back before you work this one out!
 
-Pizza.calculateToppings = () => {
-  return 1
-}
+Pizza.calculateToppings = function(...args) {
+  let result = 1
+  const argValues = Object.values(arguments)
+  // console.log(typeof argValues[0])
+  if(typeof argValues[0] === 'string') {
+    const validToppings = argValues.filter(val => toppings.includes(val)) // returns an array of toppings
+    if(validToppings.length < 4) result = 0.5 * validToppings.length
+  }
+  if(typeof argValues[0] === 'number') {
+    // check if arg values are valid
+    console.log(argValues.reduce((acc, val) => {
+        
+    }, []).length)
+  }
+  return result
+  }
+
+console.log(Pizza.calculateToppings(0, 2, 2))
 
 module.exports = Pizza;
 // console.log('Connected to constants file', bases[0])
